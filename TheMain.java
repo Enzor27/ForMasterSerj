fad090d98c2166ce1165121199d5ea853068fcef
Ruslan <Ruslan@Ruslans-MacBook-Pro.local>
2016-04-27 20:56:35 +0300

    NewCommit

--- TheMain.java
+++ TheMain.java
@@ -1,8 +1,21 @@
-/**
- * Created by Ruslan on 4/26/16.
- */
+
 public class TheMain {
-    public static void main(String []args) {
-        System.out.println("Hello world");
+
+    public static void main(String [] args) {
+        int a = 5;
+        int b = 8;
+
+        if (a==b) {
+            System.out.println("It's not possible");
+            break;
+        } else if (a!=b) {
+            System.out.println("Yes");
+            break;
+        } else if (a==b || a!=b) {
+            System.out.println("Yes it is");
+            break;
+        }else if (a!=b && b!=a) {
+            System.out.println("it's true!");
+        }
     }
 }
